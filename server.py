0d6770e2d9d76f178468de17316005b2222467b8
Lina <jilius31@list.ru>
2025-07-04 13:26:10 +0300

    upload

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from flask import Flask
 from flask import request, jsonify
 from secrets import choice
@@ -5,6 +6,24 @@ from PIL import Image
 import string
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
+
+
+MAX_FOLDER_SIZE = 1 * 1024**3      # 1 GB
+TARGET_FOLDER_SIZE = 800 * 1024**2 # 800 MB
+
+def cleanup_folder(path='images'):
+    folder = Path(path)
+    files = sorted(folder.glob('*'), key=lambda f: f.stat().st_mtime)  # oldest first
+    total_size = sum(f.stat().st_size for f in files)
+
+    while total_size > MAX_FOLDER_SIZE and files:
+        file_to_remove = files.pop(0)
+        try:
+            total_size -= file_to_remove.stat().st_size
+            file_to_remove.unlink()
+            print(f"Deleted: {file_to_remove.name}")
+        except Exception as e:
+            print(f"Failed to delete {file_to_remove.name}: {e}")
 
 alphabet = string.ascii_lowercase + string.digits
 def get_name(len = 8):
@@ -43,4 +62,4 @@ def uploaded_file(filename):
     except FileNotFoundError:
         return jsonify({'status': 'error', 'message': 'File not found'}), 404
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', 5000)
